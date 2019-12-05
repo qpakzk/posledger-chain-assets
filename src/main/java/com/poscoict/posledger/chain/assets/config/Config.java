ca52d4a4c424f0855e9a0766c9fe41b1ab11d696
yoongdoo <http://yoongdoo0819@almcm.poscoict.com>
2019-12-06 01:40:32 +0900

    getter and setter

--- src/main/java/com/poscoict/posledger/chain/assets/config/Config.java
+++ src/main/java/com/poscoict/posledger/chain/assets/config/Config.java
@@ -5,7 +5,27 @@ public class Config {
 	private static String ORG1_MSP;
 
 	private static String ORG1;
-	
+
+	private static String ADMIN;
+
+	private static String ADMIN_PASSWORD;
+
+	private static String CA_ORG1_URL;
+
+	private static String ORDERER_URL;
+
+	private static String ORDERER_NAME;
+
+	private static String CHANNEL_NAME;
+
+	private static String ORG1_PEER_0;
+
+	private static String ORG1_PEER_0_URL;
+
+	private static String EVENT_HUB;
+
+	private static String CHAINCODE_1_NAME;
+
 	public static String getORG1() {
 		return ORG1;
 	}
@@ -22,24 +42,86 @@ public class Config {
 		ORG1_MSP = org1Msp;
 	}
 
-	public static String ADMIN;
+	public static String getADMIN() {
+		return ADMIN;
+	}
 
-	public static String ADMIN_PASSWORD;
+	public static void setADMIN(String ADMIN) {
+		Config.ADMIN = ADMIN;
+	}
 
-	public static String CA_ORG1_URL;
-	
-	public static String ORDERER_URL;
-	
-	public static String ORDERER_NAME;
-	
-	public static String CHANNEL_NAME;
-	
-	public static String ORG1_PEER_0;
-	
-	public static String ORG1_PEER_0_URL;
+	public static String getAdminPassword() {
+		return ADMIN_PASSWORD;
+	}
 
-	public static String EVENT_HUB;
+	public static void setAdminPassword(String adminPassword) {
+		ADMIN_PASSWORD = adminPassword;
+	}
 
-	public static String CHAINCODE_1_NAME;
+	public static String getCaOrg1Url() {
+		return CA_ORG1_URL;
+	}
+
+	public static void setCaOrg1Url(String caOrg1Url) {
+		CA_ORG1_URL = caOrg1Url;
+	}
+
+	public static String getOrdererUrl() {
+		return ORDERER_URL;
+	}
+
+	public static void setOrdererUrl(String ordererUrl) {
+		ORDERER_URL = ordererUrl;
+	}
+
+	public static String getOrdererName() {
+		return ORDERER_NAME;
+	}
+
+	public static void setOrdererName(String ordererName) {
+		ORDERER_NAME = ordererName;
+	}
+
+	public static String getChannelName() {
+		return CHANNEL_NAME;
+	}
+
+	public static void setChannelName(String channelName) {
+		CHANNEL_NAME = channelName;
+	}
+
+	public static String getOrg1Peer0() {
+		return ORG1_PEER_0;
+	}
+
+	public static void setOrg1Peer0(String org1Peer0) {
+		ORG1_PEER_0 = org1Peer0;
+	}
+
+	public static String getOrg1Peer0Url() {
+		return ORG1_PEER_0_URL;
+	}
+
+	public static void setOrg1Peer0Url(String org1Peer0Url) {
+		ORG1_PEER_0_URL = org1Peer0Url;
+	}
+
+	public static String getEventHub() {
+		return EVENT_HUB;
+	}
+
+	public static void setEventHub(String eventHub) {
+		EVENT_HUB = eventHub;
+	}
+
+	public static String getChaincode1Name() {
+		return CHAINCODE_1_NAME;
+	}
+
+	public static void setChaincode1Name(String chaincode1Name) {
+		CHAINCODE_1_NAME = chaincode1Name;
+	}
+
+
 
 }
